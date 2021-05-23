4d51ad49236676ef17460f851057536621836e21
Snigdha <snigdha.jain30@gmail.com>
2021-05-23 17:17:34 +0530

    authentication

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'loginScreen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Could not load app'),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Phone Verification',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+                primaryColor: Colors.black,
+                primarySwatch: Colors.grey,
+                inputDecorationTheme: InputDecorationTheme(
+                    labelStyle: TextStyle(color: Colors.grey)),
+                backgroundColor: Colors.grey),
+            home: LoginScreen(),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  CircularProgressIndicator(
+                    backgroundColor: Theme.of(context).primaryColor,
+                  )
+                ],
+              )
+            ]);
+      },
+    );
+  }
+}
